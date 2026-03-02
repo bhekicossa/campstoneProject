9858054015f44189518f3a822d35ae9775210881
Bheki Cossa <bcossa@csir.co.za>
2026-03-02 11:17:38 +0200

    new axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -17,7 +17,7 @@
 import axios from "axios"; // ✅ import the npm package, not your file
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://airbnbcampstone-fb23353c2f38.herokuapp.com/api",
 });
 
 instance.interceptors.request.use((config) => {
